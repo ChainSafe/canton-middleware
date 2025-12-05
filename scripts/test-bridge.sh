1b3a7e2842c83359f9be5b0dfe1d9e933c1c558c
Peter Kalambet <peter@kalambet.dev>
2025-12-05 22:58:36 +0100

    update bridge test to accomodate new contracts

--- scripts/test-bridge.sh
+++ scripts/test-bridge.sh
@@ -24,8 +24,8 @@ NC='\033[0m' # No Color
 
 # Configuration
 ETH_RPC_URL="${ETH_RPC_URL:-http://localhost:8545}"
-BRIDGE="0x5FbDB2315678afecb367f032d93F642f64180aa3"
-TOKEN="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+BRIDGE="0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+TOKEN="0x5FbDB2315678afecb367f032d93F642f64180aa3"
 RELAYER="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 RELAYER_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 # Owner is the deployer of PromptToken and CantonBridge (has admin rights)
