4c4aa67f55014024fae884b7e7d96b75bd52341c
sadiq1971 <sadiqurr8@gmail.com>
2026-02-22 01:42:12 +0600

    added database migration support

--- pkg/apidb/dao/reconciliation_state.go
+++ pkg/apidb/dao/reconciliation_state.go
@@ -4,10 +4,10 @@ import "time"
 
 // ReconciliationStateDao is a data access object that maps directly to the 'reconciliation_state' table in PostgreSQL.
 type ReconciliationStateDao struct {
-	tableName            struct{}   `pg:"reconciliation_state"` // nolint
-	ID                   int        `json:"id" pg:",pk,default:1"`
-	LastProcessedOffset  int64      `json:"last_processed_offset" pg:",use_zero"`
-	LastFullReconcileAt  *time.Time `json:"last_full_reconcile_at,omitempty" pg:"last_full_reconcile_at"`
-	EventsProcessed      int        `json:"events_processed" pg:",use_zero"`
-	UpdatedAt            time.Time  `json:"updated_at" pg:"default:now()"`
+	tableName           struct{}   `pg:"reconciliation_state"` // nolint
+	ID                  int        `json:"id" pg:",pk"`
+	LastProcessedOffset int64      `json:"last_processed_offset" pg:",use_zero"`
+	LastFullReconcileAt *time.Time `json:"last_full_reconcile_at,omitempty" pg:"last_full_reconcile_at"`
+	EventsProcessed     int        `json:"events_processed" pg:",use_zero"`
+	UpdatedAt           time.Time  `json:"updated_at" pg:"default:now()"`
 }
